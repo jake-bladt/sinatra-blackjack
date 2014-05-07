428af03305bd600ef78ced04ef64e22be1e2ee13
Jake Bladt <jake@jakebladt.com>
2014-05-07 10:03:41 -0400

    break out script

---
+++ scripts/blackjack.js
@@ -0,0 +1,17 @@
+    $(document).ready(function() {
+       $.ajax({
+         type: 'GET',
+         dataType: 'text json',
+         url: 'http://localhost:4567/cards/3',
+         success: function(data) {
+           var $myCards = $('#myCards');
+           $(data.cards).each(function() {
+             var cardElem = "<img src='cards/" + this + ".png' />"
+             $myCards.append(cardElem);
+           });
+         },
+         error: function(xhr, status, err) {
+           alert(status + ': ' + err + '. ' + xhr.responseText);
+         }
+       });
+    });
