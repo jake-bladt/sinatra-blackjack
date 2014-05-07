dd2eb1360cc54d9f565443b2bf33ada27af344a0
Jake Bladt <jake@jakebladt.com>
2014-05-06 23:13:50 -0400

    allow cross-site scripting

--- blackjack.rb
+++ blackjack.rb
@@ -5,6 +5,8 @@ class Blackjack < Sinatra::Base
   helpers Sinatra::JSON
 
   get '/cards' do
+  	response.headers['Access-Control-Allow-Origin'] = '*'
+  	content_type :json
   	json :cards => ['Ah', 'Kd']
   end
 
