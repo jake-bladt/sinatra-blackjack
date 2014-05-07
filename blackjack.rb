81df926497656573f6c99be1a9d2e3ff575d8286
Jake Bladt <jake@jakebladt.com>
2014-05-07 00:04:18 -0400

    draw cards

--- blackjack.rb
+++ blackjack.rb
@@ -7,7 +7,7 @@ class Blackjack < Sinatra::Base
   get '/cards' do
   	response.headers['Access-Control-Allow-Origin'] = '*'
   	content_type :json
-  	json :cards => ['Ah', 'Kd']
+  	json :cards => ['AH', '9C']
   end
 
 end
