9e7dc42fe8e90bca02f6e521c2ea06a5d2fea0b1
Jake Bladt <jake@jakebladt.com>
2014-05-07 09:20:17 -0400

    random cards

--- blackjack.rb
+++ blackjack.rb
@@ -5,12 +5,21 @@ class Blackjack < Sinatra::Base
   helpers Sinatra::JSON
 
   before do
+    @faces = "A23456789TJQK".scan(/./)
+    @suits = "CDHS".scan(/./)
+
   	response.headers['Access-Control-Allow-Origin'] = '*'
   	content_type :json
   end
 
   get '/cards' do
-  	json :cards => ['AH', '9C']
+  	cards = []
+  	2.times { cards.push get_card }
+  	json :cards => cards
+  end
+
+  def get_card
+    @faces.sample + @suits.sample
   end
 
 end
